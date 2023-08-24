9ed7e4a777a3478f01e5a828391055d657d01cda
matheusjean <matheusjean11@hotmail.com>
2023-08-24 11:37:01 -0300

    chore: next config to use images link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true
+  },
+  images: {
+    domains: ['gamearena.gg', 'localhost', 'noticias.maisesports.com.br']
+  },
+  output: 'standalone'
+}
 
 module.exports = nextConfig

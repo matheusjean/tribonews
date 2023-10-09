6dc564b1a5b4fd16723531faecd567450d8f036b
matheusjean <matheusjean11@hotmail.com>
2023-10-09 17:59:59 -0300

    chore: delete imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Header from 'components/Header';
-import Hero from 'components/Hero';
-import Popular from 'components/MainContent';
 import Teste from './teste/page';
 import Homes from 'components/Homes';
 

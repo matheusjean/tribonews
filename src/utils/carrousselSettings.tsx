a3e001ae361f1a1a3e8c1dfea617c3a83bc9d687
matheusjean <matheusjean11@hotmail.com>
2023-10-09 10:35:06 -0300

    feat: move carrousselSettings to util folder

---
+++ src/utils/carrousselSettings.tsx
@@ -0,0 +1,21 @@
+
+export const CarrousselSettings = {
+  className: "center",
+  centerMode: false,
+  infinite: true,
+  centerPadding: '0',
+  slidesToShow: 2,
+  speed: 500,
+  rows: 3,
+  slidesPerRow: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 4,
+      },
+    },
+  ],}

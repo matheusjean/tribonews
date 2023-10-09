9e21988b15282408e305bdfb26cd82fc1f17bf8b
matheusjean <matheusjean11@hotmail.com>
2023-10-09 19:33:39 -0300

    feat: add context use to slug

--- src/app/news/[...slug]/page.tsx
+++ src/app/news/[...slug]/page.tsx
@@ -1,30 +1,14 @@
 "use client"
-import react from 'react'
-import { Heading2 } from 'components/Heading2'
-import { MainContainer } from 'components/MainContainer'
-import { PostContainer } from 'components/PostContainer'
-import { PostCover } from 'components/PostCover'
-import { PostDetails } from 'components/PostDetails'
-import { useEffect, useState } from 'react'
-import { getNewsByHat } from 'services/Requests'
-import { useParams } from 'next/navigation'
+import React from 'react';
+import { Heading2 } from 'components/Heading2';
+import { MainContainer } from 'components/MainContainer';
+import { PostContainer } from 'components/PostContainer';
+import { PostCover } from 'components/PostCover';
+import { PostDetails } from 'components/PostDetails';
+import { usePost } from 'context/NewsContext';
 
 export default function Post() {
-  const [news, setNews] = useState<News>()
-
-  const hat = useParams()
-
-  console.log(hat.slug[1])
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await getNewsByHat(hat.slug[1]);
-      console.log('Resposta da API:', response);
-      setNews(response);
-      return response
-    }
-    fetchData()
-  }, [])
+  const { news } = usePost();
 
   return (
     <>
@@ -35,5 +19,5 @@ export default function Post() {
         <PostContainer content={news?.text} />
       </MainContainer>
     </>
-  )
+  );
 }

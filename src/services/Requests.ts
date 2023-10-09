a891d35b9390d1fb6adad9afdffcfc6556fb2e63
matheusjean <matheusjean11@hotmail.com>
2023-10-09 10:34:33 -0300

    feat: request for newsById and byHat

--- src/services/Requests.ts
+++ src/services/Requests.ts
@@ -4,3 +4,15 @@ export const getNews = async () => {
   const response = await (await httpRequest.get(`/news`)).data
   return response
 }
+
+export const getNewsById = async (newsId: string) => {
+  const news = await (await httpRequest.get(`news/${newsId}`)).data
+
+  return news
+}
+
+export const getNewsByHat = async (hat: any) => {
+  const news = await (await httpRequest.get(`/news/by-hat/${hat}`)).data
+
+  return news
+}

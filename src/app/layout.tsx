a59ad193d8c15dc113c4be5de049860df47898bf
matheusjean <matheusjean11@hotmail.com>
2023-10-09 19:33:23 -0300

    feat: add context on root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+"use client"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ThemeProvider from './theme-provider'
 import GlobalStyles from '../styles/global'
-
+import { PostProvider } from 'context/NewsContext'
+import { useParams } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,12 +18,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const hat = useParams()
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>
-          <GlobalStyles />
+          <PostProvider hat={hat?.slug?.[1] as any}>
+            <GlobalStyles />
             {children}
+          </PostProvider>
         </ThemeProvider>
       </body>
     </html>
